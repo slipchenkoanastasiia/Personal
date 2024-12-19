e99c0ae11106b04756b8a7aef42d56de3b512bb0
slipchenkoanastasiia <138067087+slipchenkoanastasiia@users.noreply.github.com>
2024-12-20 00:45:44 +0200

    fix media

--- dist/index.2b039d63.js
+++ dist/index.2b039d63.js
@@ -1,8 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     const languageSelect = document.getElementById('language-select');
+    const languageIcon = document.getElementById('language-toggle');
     const content = document.querySelector('.content');
+    languageIcon.addEventListener('click', ()=>{
+        languageSelect.classList.toggle('active');
+    });
     languageSelect.addEventListener('change', (event)=>{
         const selectedLanguage = event.target.value;
+        console.log('Selected Language: ', selectedLanguage);
         const langElements = content.querySelectorAll('.lang');
         langElements.forEach((element)=>{
             element.classList.remove('active');
@@ -10,8 +15,9 @@ document.addEventListener('DOMContentLoaded', function() {
         const activeLang = content.querySelector(`.lang[data-lang="${selectedLanguage}"]`);
         if (activeLang) {
             activeLang.classList.add('active');
-            console.log(`Switched language to: ${selectedLanguage}`);
-        } else console.error(`Language not found: ${selectedLanguage}`);
+            console.log('Language switched to: ', selectedLanguage);
+        } else console.warn(`Language not found: ${selectedLanguage}`);
+        languageSelect.classList.remove('active');
     });
 });
 
--- docs/js/language.js
+++ docs/js/language.js
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
-document.addEventListener('DOMContentLoaded', function () {
-  const languageSelect = document.getElementById('language-select');
-  const content = document.querySelector('.content');
-
-  languageSelect.addEventListener('change', (event) => {
-    const selectedLanguage = event.target.value;
-=======
 document.addEventListener('DOMContentLoaded', function() {
-  const languageSelect = document.getElementById('language-select');
-  const languageIcon = document.getElementById('language-toggle');
-  const content = document.querySelector('.content');
+  const languageSelect = document.getElementById('language-select'); 
+  const languageIcon = document.getElementById('language-toggle'); 
+  const content = document.querySelector('.content'); 
 
   languageIcon.addEventListener('click', () => {
     languageSelect.classList.toggle('active'); 
@@ -18,26 +10,21 @@ document.addEventListener('DOMContentLoaded', function() {
   languageSelect.addEventListener('change', (event) => {
     const selectedLanguage = event.target.value;
     console.log('Selected Language: ', selectedLanguage);
->>>>>>> 1bd78ee95345c88baf117d73f35fc87e45bb315c
 
     const langElements = content.querySelectorAll('.lang');
     langElements.forEach(element => {
-      element.classList.remove('active');
+      element.classList.remove('active'); 
     });
 
     const activeLang = content.querySelector(`.lang[data-lang="${selectedLanguage}"]`);
     if (activeLang) {
-      activeLang.classList.add('active');
-<<<<<<< HEAD
-      console.log(`Switched language to: ${selectedLanguage}`);
-    } else {
-      console.error(`Language not found: ${selectedLanguage}`);
-    }
-=======
+      activeLang.classList.add('active'); 
       console.log('Language switched to: ', selectedLanguage);
+    } else {
+      console.warn(`Language not found: ${selectedLanguage}`); 
     }
 
     languageSelect.classList.remove('active');
->>>>>>> 1bd78ee95345c88baf117d73f35fc87e45bb315c
   });
+
 });
