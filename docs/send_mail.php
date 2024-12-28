ed09b64a348d08a707800fc5c3b7b602bfad6fb3
slipchenkoanastasiia <138067087+slipchenkoanastasiia@users.noreply.github.com>
2024-12-28 23:05:30 +0200

    fix bug

--- docs/send_mail.php
+++ docs/send_mail.php
@@ -1,24 +1,19 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Змінні з форми
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
     $message = htmlspecialchars($_POST['message']);
 
-    // Email, на який відправляти повідомлення
     $to = "slipchenko.as@gmail.com";
     $subject = "Нове повідомлення з форми";
 
-    // Тіло листа
     $body = "Ім'я: $name\n";
     $body .= "Email: $email\n";
     $body .= "Повідомлення:\n$message\n";
 
-    // Заголовки
     $headers = "From: $email\r\n";
     $headers .= "Reply-To: $email\r\n";
 
-    // Відправка
     if (mail($to, $subject, $body, $headers)) {
         echo "Повідомлення успішно відправлено!";
     } else {
